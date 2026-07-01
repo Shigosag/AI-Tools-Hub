80e4f55686710a28b99bf20868128d8af87dd920
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-07-01 16:50:51 +0000

    Update Dashboard.tsx

--- client/components/Dashboard.tsx
+++ client/components/Dashboard.tsx
@@ -47,7 +47,7 @@ export default function Dashboard() {
           </span>
         </motion.p>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {tools.map((tool) => (
             <motion.div
               key={tool.title}
